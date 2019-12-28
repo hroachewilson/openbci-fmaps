8d77d74bf8cc313055b5d9ecff381cd34d07f9b0
Mars Xerxes <hroachewilson@gmail.com>
2019-12-28 22:38:01 +1100

    image works

--- run.py
+++ run.py
@@ -27,31 +27,18 @@ HM_SECONDS = 10  # this is approximate. Not 100%. do not depend on this.
 TOTAL_ITERS = HM_SECONDS*25  # ~25 iters/sec
 
 while True:
-    sample, timestamp = inlet.pull_sample()
-    sample = np.array(sample[:high_band])
-    print(sample)
-    sample[:] = 255.0 * (sample[:] / max(sample.max() - sample.min(), 0.001))
-    channel_data = []
-    image = np.zeros((4,4,3), np.uint8)
-    print(sample)
 
-    for i in range(4):
-        for j in range(4):
-            #image[i, j, 0] = 0
-            #print(i,j)
-            #image[i, j, 0] = 255
-            #image[i, j, 1] = 0
-            #image[i, j, 2] = 0
-            image[i, j, 0] = int(sum(sample[0:low_band]) / band_width)
-            image[i, j, 1] = int(sum(sample[low_band:mid_band]) /  band_width)
-            image[i, j, 2] = int(sum(sample[mid_band:high_band]) / band_width)
-            print(sample[0:low_band])
-            print(sample[low_band:mid_band])
-            print(sample[mid_band:high_band])
-            print(i,j, " has ", int(sum(sample[0:low_band]) / band_width),
-                                int(sum(sample[low_band:mid_band]) /  band_width),
-                                int(sum(sample[mid_band:high_band]) / band_width))
-            #print(i, j)
+    image = np.zeros((4,4,3), np.uint8)
+    for i in range(16):
+        sample, timestamp = inlet.pull_sample()
+        sample = np.array(sample[:high_band])
+        sample_range = sample.max() - sample.min()
+        sample[:] = sample[:] / sample_range
+
+        image[int(i / 4), i % 4, 0] = int(255.0 * sum(sample[0:low_band]) / band_width)
+        image[int(i / 4), i % 4, 1] = int(255.0 * sum(sample[low_band:mid_band]) /  band_width)
+        image[int(i / 4), i % 4, 2] = int(255.0 * sum(sample[mid_band:high_band]) / band_width)
 
     cv2.imshow('', cv2.resize(image, (800,600), interpolation=cv2.INTER_CUBIC))
-    cv2.waitKey(0)
+    #cv2.imshow('', image)
+    cv2.waitKey(1)
